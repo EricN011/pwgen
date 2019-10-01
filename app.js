037774c9e433b4f1ef91e4bcef7551d517396622
Eric Noll <eanoll88@gmail.com>
2019-09-30 20:23:15 -0500

    pwgen update

---
+++ app.js
@@ -0,0 +1,11 @@
+var pw = "dynopass";
+// defined function
+function showAlert() {
+  alert("you clicked me!");
+  pwBox.innerHTML = pw;
+}
+
+// genBtn.addEventListener("click", showAlert);
+
+// change this pw into dynamic random pw
+// document.write(pw);
