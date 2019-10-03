a17a1d5168b1c2cbfa684bc8a4f20769cd56a910
Eric Noll <eanoll88@gmail.com>
2019-10-02 20:47:10 -0500

    pwgen update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var pw = [];
 var charLen = prompt("How many characters would you like for your password?");
-// charLen = parseInt(charLen, 10);
+charLen = parseInt(charLen, 10);
 var charSpec = [
   "(",
   "[",
@@ -18,7 +18,33 @@ var charSpec = [
   ")"
 ];
 var charNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var charLow = [
+var charLet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
   "a",
   "b",
   "c",
@@ -46,56 +72,8 @@ var charLow = [
   "y",
   "z"
 ];
-var charUpp = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
-];
-// var isCharSpec = true;
-// var isCharNum = true;
-
-var charChoice = [];
-
-var hasCharNum = confirm("Numbers?");
-var hasCharSpec = confirm("Special Characters?");
-var hasCharUpp = confirm("Uppercase Characters?");
-var hasCharLow = confirm("Lowercase Characters?");
-
-if (hasCharSpec) {
-  charChoice = charChoice.concat(charSpec);
-}
-if (hasCharNum) {
-  charChoice = charChoice.concat(charNum);
-}
-if (hasCharUpp) {
-  charChoice = charChoice.concat(charUpp);
-}
-if (hasCharLow) {
-  charChoice = charChoice.concat(charLow);
-}
+var isCharSpec = true;
+var isCharNum = true;
 
 function shuffle(array) {
   var currentIndex = array.length,
@@ -120,34 +98,58 @@ function shuffle(array) {
 /* defined function */
 function createPassword() {
   for (i = 0; i < charLen; i++) {
-    var randomSymNo = Math.floor(Math.random() * charChoice.length);
-
-    /* inject a symbol in the string */
-    pw.push(charChoice[randomSymNo]);
-    console.log(pw);
+    if (isCharSpec === true && i === 0) {
+      var randomSymNo = Math.floor(Math.random() * charSpec.length);
+      /* inject a symbol in the string */
+      pw.push(charSpec[randomSymNo]);
+    } else {
+      /* inject a uppercase letter into the string */
+      var randomNo = Math.floor(Math.random() * charLet.length);
+      pw.push(charLet[randomNo]);
+    }
   }
+  /* randomize array & convert into a string */
+  pwShuffle = shuffle(pw);
+  var newPass = "";
+  for (i = 0; i < pwShuffle.length; i++) {
+    newPass = newPass + pwShuffle[i];
+  }
+  pwBox.innerHTML = newPass;
 }
-/* randomize array & convert into a string */
-pwShuffle = shuffle(pw);
-document.getElementById("pwBox").innerHTML = pw;
-var newPass = "";
-for (i = 0; i < pwShuffle.length; i++) {
-  newPass = newPass + pwShuffle[i];
-}
-pwBox.innerHTML = newPass;
 
 /* first event */
 // change this pw into a dynamic random pw
-document.write(pw);
+//document.write(pw);
+// defined function
+// function showPrompt() {
+// for (i = 0; i < charLength; i++) {
+// var randomNum;
+// }
+// charSpec = confirm("Would you like to use special characters?");
+// charNum = confirm("Would you like to use any numeric characters?");
+// charLow = confirm("Would you like to use lowercase characters?");
+// charUpp = confirm("Would you like to use uppercase characters?");
+// pwBox.innerHTML = pw;
+// }
+
+// genBtn.addEventListener("click", showAlert);
+
+// number of characters for the password (between 8 and 128)
+// if (charLength < 8) {
+//   alert("password must be longer");
+// }
+// special characters
+// numeric characters
+// lowercase characters
+// uppercase characters
+
+// change this pw into dynamic random pw
+// document.write(pw);
 
 // console log
 console.log(charLen);
 console.log(typeof charLen);
 console.log(charSpec);
 console.log(charNum);
-console.log(charLow);
-console.log(charUpp);
-
-// /* inject a uppercase letter into the string */
-// var randomNo = Math.floor(Math.random() * charChoice.length);
-// pw.push(charUpp[randomCharUpp]);
+console.log(charLet);
+console.log(pw);
